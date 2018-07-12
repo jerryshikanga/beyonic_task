eceaba0146e11ae16044fb11f903e0c4335e6dfd
Jerry Shikanga <jerryshikanga@gmail.com>
2018-07-12 20:35:15 +0300

    Unique username email check on register

--- accounts/forms.py
+++ accounts/forms.py
@@ -10,27 +10,33 @@ class RegisterForm(forms.Form):
     first_name = forms.CharField(max_length=50, required=True)
     last_name = forms.CharField(max_length=50, required=True)
     email = forms.EmailField()
-    telephone = forms.CharField(max_length =999)
+    telephone = forms.CharField(max_length=999)
     password1 = forms.CharField(label=_("Password"), widget=forms.PasswordInput)
-    password2 = forms.CharField(label=_("Password confirmation"), widget=forms.PasswordInput, help_text=_("Enter the same password as above, for verification."))
+    password2 = forms.CharField(label=_("Password confirmation"), widget=forms.PasswordInput,
+                                help_text=_("Enter the same password as above, for verification."))
 
     def clean_password2(self):
-        def clean_password2(self):
-            password1 = self.cleaned_data.get("password1")
-            password2 = self.cleaned_data.get("password2")
-            if password1 and password2 and password1 != password2:
-                raise forms.ValidationError( "The two password fields didn't match.", code='password_mismatch',)
-            return password2
+        password1 = self.cleaned_data.get("password1")
+        password2 = self.cleaned_data.get("password2")
+        if password1 and password2 and password1 != password2:
+            raise forms.ValidationError("The two password fields didn't match.", code='password_mismatch', )
+        return password2
+
+    def clean_email(self):
+        email = self.cleaned_data.get('email')
+        if email and User.objects.filter(email=email).exists():
+            raise forms.ValidationError('Email addresses must be unique.')
+        return email
 
     def save(self):
         form_data = self.data
         user = User.objects.create(
-            email= form_data.get("email"),
-            first_name= form_data.get("first_name"),
-            last_name= form_data.get("last_name"),
-            username= form_data.get("email")
+            email=form_data.get("email"),
+            first_name=form_data.get("first_name"),
+            last_name=form_data.get("last_name"),
+            username=form_data.get("email")
         )
-        user.set_password(form_data.get("password"))
+        user.set_password(form_data.get("password2"))
         user.save()
 
         client = nexmo.Client(key=settings.NEXMO_API_KEY, secret=settings.NEXMO_API_SECRET)
